c7ff74519d8bc6aa056c0eab13855ec072081cfc
Blanket25 <bianca_inga@hotmail.com>
2021-12-02 15:15:27 +0100

    corrections on database

--- tidyAppScript.sql
+++ tidyAppScript.sql
@@ -3,27 +3,31 @@ drop table if exists users;
 drop table if exists task;
 drop table if exists task_list;
 
+create database tidyApp;
+
 create table tidy_group (
 	id serial primary key,
 	group_name varchar(60) not null,
 	date_of_creation date not null
 )
 
+CREATE TYPE type AS ENUM ('roomie', 'admin');
 create table users (
 	id serial primary key,
-	name varchar(60) not null,
+	username varchar(60) not null,
 	email varchar(120) not null,
-	roomie_admin varchar(30) not null,
+	type_of_user type not null,
 	group_id int references tidy_group(id)
 )
 
+CREATE TYPE frequency AS ENUM ('weekly', 'biweekly', 'monthly');
 create table task (
 	id serial primary key,
 	name varchar(60) not null,
 	task_completed boolean not null,
 	description varchar(500),
 	starting_date date not null,
-	frequency varchar(60) not null,
+	frequency frequency not null,
 	user_id int references users(id)
 )
 
