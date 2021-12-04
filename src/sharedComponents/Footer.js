6d116a5e89ef188c554b1d0cfb74fa6c5d91d6ba
Elmira Saifullina <elya@Elmiras-MacBook-Air.local>
2021-12-04 16:45:39 +0100

    created responsive homepage and footer

---
+++ src/sharedComponents/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+	return (
+		<footer className='footer u-center-text'>
+			<div className='footer-text '>
+				<p>Happy code by Team May2</p>
+			</div>
+		</footer>
+	);
+};
+export default Footer;
