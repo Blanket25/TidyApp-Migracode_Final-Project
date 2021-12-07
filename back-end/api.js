ff5de2bb048c922a6ad54ccc324be5b179ab7b16
Omar <omared250@gmail.com>
2021-12-07 19:45:52 +0100

    adding addNewTask function

--- back-end/api.js
+++ back-end/api.js
@@ -55,6 +55,31 @@ const api = () => {
     return await res.status(200).json(taskList.rows);
   };
 
+  const addNewTask = async (req, res) => {
+    // require all the elements into the body
+    const newTask = req.body;
+
+    // checking if teh task already exists
+    const itExists = await connection.query('select * from task where name=$1', [newTask.name])
+    if (itExists.rows.length > 0) {
+      return res.status(400).send('The task already exists try updating the task instead of creat a new one!')
+    } else { 
+      // if not create the task
+      const createTask = `insert into task (name, task_completed, description, starting_date, frequency, user_id) returning id`
+      const newTaskRow = await connection.query(createTask,
+      [
+        newTask.name,
+        newTask.task_completed,
+        newTask.description,
+        newTask.starting_date,
+        newTask.frequency,
+        newTask.user_id
+      ])
+      // answering wuth the task id
+      await res.status(200).json({taskId : newTaskRow.rows[0].id});
+    }
+  }
+
   return {
     getUsers,
     addUser,
