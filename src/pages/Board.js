062e6f36b2ab4a9c60fc3c9094de38de29702d4c
Omar <omared250@gmail.com>
2021-12-12 22:32:02 +0100

    print print option working

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -2,26 +2,34 @@ import "../index.css";
 import Nav from "./sharedComponents/Nav";
 import Footer from "../pages/sharedComponents/Footer";
 import printer from "../img/printer.svg";
+import ContentToPrint from "./ContentToPrint";
+import React, { useRef } from 'react';
+import ReactToPrint from "react-to-print";
 
 function Board() {
+  class ComponentToPrint extends React.Component {
+    render() {
+      return (
+        <div>
+          <ContentToPrint/>
+        </div>
+      )
+    }
+  }
+
+  const componentRef = useRef();
+  
   return (
     <div>
       <div className="board-header">
         <Nav />
-        <button className="purple-btn print-btn">
+        <ReactToPrint
+          trigger={() => <button className="purple-btn print-btn">
           <img src={printer} alt="printer icon" />
-          <p>Print it!</p>
-        </button>
+          <p>Print it!</p> </button>}
+          content={() => componentRef.current} />
       </div>
-      <div className="card-container">
-        <div className="card">
-          <p>Nombre</p>
-          <div>
-            <p>Tarea</p>
-          </div>
-          <input type="checkbox" />
-        </div>
-      </div>
+      <ComponentToPrint ref={componentRef}/>
       <Footer />
     </div>
   );
