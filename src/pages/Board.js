58ddc68fc6a84f756b7dd89978c57747338759b2
Blanket25 <bianca_inga@hotmail.com>
2021-12-14 15:12:47 +0100

    working board

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,62 +1,9 @@
-
-import '../index.css';
-import Nav from './sharedComponents/Nav';
-import Footer from '../pages/sharedComponents/Footer';
-import printer from '../img/printer.svg';
-import { useState } from 'react';
-
-function Board() {
-	const tasks = [
-		{
-			name: 'Elmira',
-			task: 'clean the kitchen',
-		},
-		{
-			name: 'Omar',
-			task: 'buy food',
-		},
-		{
-			name: 'Bianca',
-			task: 'clean the living room',
-		},
-	];
-	const handleChange = (index) => {
-		console.log(`changed task ${index}`);
-	};
-	return (
-		<div>
-			<div className='board-header'>
-				<Nav />
-				<button className='purple-btn print-btn'>
-					<img src={printer} alt='printer icon' />
-					<p>Print it!</p>
-				</button>
-			</div>
-			<div className='card-container'>
-				<div className='card'>
-					{tasks.map((item, index) => {
-						return (
-							<div className='task-container' key={index} id={index}>
-								<p>{item.name}</p>
-								<div>
-									<p>{item.task}</p>
-								</div>
-								<input type='checkbox' onChange={() => handleChange(index)} />
-							</div>
-						);
-					})}
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
-
 import "../index.css";
 import Nav from "./sharedComponents/Nav";
 import Footer from "../pages/sharedComponents/Footer";
 import printer from "../img/printer.svg";
 import ContentToPrint from "./ContentToPrint";
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import ReactToPrint from "react-to-print";
 
 function Board() {
@@ -64,29 +11,32 @@ function Board() {
     render() {
       return (
         <div>
-          <ContentToPrint/>
+          <ContentToPrint />
         </div>
-      )
+      );
     }
   }
 
   const componentRef = useRef();
-  
+
   return (
     <div>
       <div className="board-header">
         <Nav />
         <ReactToPrint
-          trigger={() => <button className="purple-btn print-btn">
-          <img src={printer} alt="printer icon" />
-          <p>Print it!</p> </button>}
-          content={() => componentRef.current} />
+          trigger={() => (
+            <button className="purple-btn print-btn">
+              <img src={printer} alt="printer icon" />
+              <p>Print it!</p>{" "}
+            </button>
+          )}
+          content={() => componentRef.current}
+        />
       </div>
-      <ComponentToPrint ref={componentRef}/>
+      <ComponentToPrint ref={componentRef} />
       <Footer />
     </div>
   );
-
 }
 
 export default Board;
--- src/pages/ContentToPrint.js
+++ src/pages/ContentToPrint.js
@@ -1,3 +1,5 @@
+import "../index.css";
+
 function ContentToPrint() {
   const tasks = [
     {
