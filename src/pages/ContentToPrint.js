243218b75adcf6a0b02efa67f06123ffb09d373a
Blanket25 <bianca_inga@hotmail.com>
2021-12-14 15:02:30 +0100

    conflicts resolved

--- src/pages/ContentToPrint.js
+++ src/pages/ContentToPrint.js
@@ -1,15 +1,38 @@
 function ContentToPrint() {
-    return (
+  const tasks = [
+    {
+      name: "Elmira",
+      task: "clean the kitchen",
+    },
+    {
+      name: "Omar",
+      task: "buy food",
+    },
+    {
+      name: "Bianca",
+      task: "clean the living room",
+    },
+  ];
+  const handleChange = (index) => {
+    console.log(`changed task ${index}`);
+  };
+  return (
     <div className="card-container">
-        <div className="card">
-        <p>Nombre</p>
-        <div>
-            <p>Tarea</p>
-        </div>
-        <input type="checkbox" />
-        </div>
+      <div className="card">
+        {tasks.map((item, index) => {
+          return (
+            <div className="task-container" key={index} id={index}>
+              <p>{item.name}</p>
+              <div>
+                <p>{item.task}</p>
+              </div>
+              <input type="checkbox" onChange={() => handleChange(index)} />
+            </div>
+          );
+        })}
+      </div>
     </div>
-    )
+  );
 }
 
 export default ContentToPrint;
