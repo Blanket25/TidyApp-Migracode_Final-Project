2a2d09291d7b1056ccc154684d684f725c747dbf
Blanket25 <bianca_inga@hotmail.com>
2021-12-31 15:49:26 +0100

    admin css added and new features in login

--- src/pages/sharedComponents/Header.js
+++ src/pages/sharedComponents/Header.js
@@ -1,23 +1,30 @@
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { isAuthenticated, logOut } from "../../auth";
+//import { isAuthenticated, logOut } from "../../auth";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
   const [isLogged, setIsLogged] = useState(false);
-  const navigate=useNavigate();
-  const idFromStorage=window.localStorage.getItem("groupId")
+  const navigate = useNavigate();
+  const idFromStorage = window.localStorage.getItem("groupId");
 
   useEffect(() => {
-    setIsLogged(isAuthenticated);
-  }, []);
+    if (idFromStorage !== null) setIsLogged(true);
+  }, [idFromStorage]);
 
-  const goToBoard=()=>{
-    if(idFromStorage.length>0){navigate(`/board/${idFromStorage}`,{ state: { idFromStorage }}
-    )}}
-   
-  
+  const goToBoard = () => {
+    if (idFromStorage.length > 0) {
+      navigate(`/board/${idFromStorage}`, { state: { idFromStorage } });
+    }
+  };
+
+  const goToAdminPanel = () => {
+    if (idFromStorage.length > 0) {
+      navigate("adminpanel", { state: { idFromStorage } });
+    }
+  };
+
   return (
     <header>
       <div className="header-nav-container">
@@ -31,14 +38,16 @@ function Header() {
               to="/"
               onClick={() => {
                 setIsLogged(false);
-                logOut();
               }}
             >
               Log out{" "}
             </Link>
-           
+
             <button className="orange-btn" onClick={goToBoard}>
               Go to board
+            </button>
+            <button className="orange-btn" onClick={goToAdminPanel}>
+              Admin Panel
             </button>
           </>
         ) : (
